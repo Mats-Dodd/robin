91fd72a8e9d43b03e830ae021423d61891c957f9
Mats Dodd <mats.dodd12@gmail.com>
2025-04-29 22:58:03 -0700

    we have a sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import Editor from "@/components/editor/editor";
-
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import "./index.css";
 
 const App = () => {
   return (
-    <div className="card">
-      <Editor />
-    </div>
-  )
-}
+    <SidebarProvider className="flex min-h-screen">
+      <AppSidebar />
+      <main className="flex-1 p-4">
+        <SidebarTrigger />
+        <div className="content">
+          {/* Your page content goes here */}
+        </div>
+      </main>
+    </SidebarProvider>
+  );
+};
 
 export default App
